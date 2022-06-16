48d94123561601208a9da7fdcb6021062c2bebcb
datp01 <97713597+datp01@users.noreply.github.com>
2022-06-16 12:37:08 +0700

    Chinh sua

--- Baithi/Ex2/src/Flight.java
+++ Baithi/Ex2/src/Flight.java
@@ -1,12 +1,31 @@
-public class Flight {
-    private String destination;
-    private int number;
 
-    public Flight(String destination, int number) {
-        this.destination = destination;
+import java.util.Scanner;
+
+public class Flight {
+    int number;
+    String destination;
+    public Flight(int number, String destination) {
         this.number = number;
+        this.destination = destination;
     }
 
+    public Flight() {
+    }
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Destination: "); String destination = sc.nextLine();
+        System.out.print("Enter Number: "); int number = sc.nextInt();
+        Flight flight = new Flight(number, destination);
+        if (number == 0 || destination == null) {
+            System.err.println("Number should be positive-valued.");
+        } else {
+            System.out.println("Succesfull");
+            this.destination=destination;
+            this.number=number;
+        }
+    }
     public String getDestination() {
         return destination;
     }
@@ -17,4 +36,5 @@ public class Flight {
     public void display(){
         System.out.println(getNumber() + "," + getDestination() );
     }
+
 }
